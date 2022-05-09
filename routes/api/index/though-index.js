bf367706cbde1a9802fc501083f6bda0591b75ac
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-08 23:53:45 -0700

    added module exports for router and other routers that were required

--- routes/api/index/though-index.js
+++ routes/api/index/though-index.js
@@ -8,3 +8,13 @@ const {
   addReaction,
   removeReaction,
 } = require('../../controllers/thought-controller');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
