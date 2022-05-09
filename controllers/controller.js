a8bccf6765509e4a99a912c55de765d2ae57f117
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-08 23:34:46 -0700

    added a const controller and a err staus 500

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,14 @@
 const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
