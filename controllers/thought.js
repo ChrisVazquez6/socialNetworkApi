c1b3c8bc894e35d83a19b1bfd67c4862ad718dd9
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-08 23:40:45 -0700

    added get user to add user if succes staus 500

--- controllers/thought.js
+++ controllers/thought.js
@@ -1,1 +1,15 @@
 const { User, Thought } = require('../models');
+
+const userController = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .select('-__v')
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
